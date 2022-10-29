72126dfec7952b021f2f1842365cd4a966619ba6
“JaswanthAbbineni” <jaswanth.abbineni@gmail.com>
2022-10-29 15:31:20 +0000

    Commiting Stash onto Master

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ class Hello
 {
 Public static void main(String args[])
 {
-System.out.println(“HELLO and THIS IS A FEATURE-101 file and LocalGITV3”);
+System.out.println(“HELLO and THIS IS FOR STASHING”);
 }
 }
 
